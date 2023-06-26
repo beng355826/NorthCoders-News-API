7ce5fa0e5fc9167634d668163f33a0995d589ab7
beng355826 <benjamin-green@live.co.uk>
2023-06-26 15:26:38 +0100

    complete challenge 2 - feedback

--- controllers/get-api-topics.controller.js
+++ controllers/get-api-topics.controller.js
@@ -5,7 +5,6 @@ const getTopics = (req, res) => {
     
  return selectTopics().then((data) =>{
     const response = {}
-    response.msg = "200 - request successful"
     response.AllTopics = data
 
 res.status(200).send(response)
