584587c1d2cd4d48f3c3f012850adf77a395f11b
beng355826 <benjamin-green@live.co.uk>
2023-06-28 09:55:40 +0100

    will complete challenge 6 feedback

---
+++ models/post-comment.model.js
@@ -0,0 +1,21 @@
+const db = require("../db/connection")
+
+exports.insertComment = (articleId, postComment) => {
+
+return db.query(
+
+`INSERT INTO comments
+(body,author, article_id) VALUES ($1, $2, $3)
+RETURNING *; `,
+[postComment.body ,postComment.username, articleId]
+
+).then((comment) => {
+
+    console.log(comment)
+
+return comment.rows[0]
+
+})
+
+
+}
