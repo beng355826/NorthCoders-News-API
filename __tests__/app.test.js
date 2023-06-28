9ac9533bb5daa681466c325709318b25293787d1
beng355826 <benjamin-green@live.co.uk>
2023-06-28 09:53:20 +0100

    will complete challenge 6 feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -156,7 +156,7 @@ describe("GET /api/articles/:article_id/comments", () => {
                 expect(comment).toHaveProperty('created_at', expect.any(String))
                 expect(comment).toHaveProperty('author', expect.any(String))
                 expect(comment).toHaveProperty('body', expect.any(String))
-                expect(comment).toHaveProperty('article_id', expect.any(Number))
+                expect(comment).toHaveProperty('article_id', 3)
             })
         })
     })
