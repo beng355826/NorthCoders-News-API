e2c13eaa7b481a87175f7bff2076d50a2e6961ff
beng355826 <benjamin-green@live.co.uk>
2023-06-29 12:51:11 +0100

    .

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development' || 'production';
+const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+  path: `${__dirname}/../.env.${ENV}`});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
