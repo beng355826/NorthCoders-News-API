229964f2003fc0a7af6b6b734a7249ca432299b6
beng355826 <benjamin-green@live.co.uk>
2023-10-03 18:24:39 +0100

    pre routing

--- controllers/get-All-Articles.controller.js
+++ controllers/get-All-Articles.controller.js
@@ -1,11 +1,8 @@
-const db = require("../db/connection")
 const {selectAllArticles} = require("../models/get-All-Articles.model")
 
 const getAllArticles = (req , res, next) => {
 
-
 const queryBody = req.query
-
 
     return selectAllArticles(queryBody).then((articles) => {
         res.status(200).send({articles})
